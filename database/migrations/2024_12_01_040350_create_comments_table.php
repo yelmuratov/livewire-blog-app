4bd02cdc2fefc261d5dcb349f1666b8f40a47fe6
yelmuratov <yelimuratovsalimbay@gmail.com>
2024-12-01 09:09:30 +0500

    Migrations created

---
+++ database/migrations/2024_12_01_040350_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade'); // References posts table
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // References users table
+            $table->text('content');
+            $table->foreignId('parent_comment_id')->nullable()->constrained('comments')->onDelete('cascade'); // Self-referencing
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
