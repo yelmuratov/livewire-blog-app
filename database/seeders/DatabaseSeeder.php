a62519437204f2cb349386ee79dd4c65be00a5e7
yelmuratov <yelimuratovsalimbay@gmail.com>
2024-12-03 07:13:34 +0500

    Seeders implemented

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,11 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Models\Like;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,5 +24,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        Category::factory(10)->create();
+        Post::factory(10)->create();
+        Comment::factory(10)->create();
+        Like::factory(10)->create();
     }
 }
